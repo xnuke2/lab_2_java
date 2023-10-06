c1ceee005fac8aeb4467d5ccccdf67d5eb1424a0
Максим <ryazanskiy.maxim@yandex.ru>
2023-10-06 20:39:00 +0300

    Добавили проверку на несколько email

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 import java.util.regex.*;
 public class Main {
     public static boolean Check_email(String email){
-        // c помощью регулярного выражения проверят являится ли данная строка email
-        String regex = "(\\w+)([\\.-]?\\w+)*@(\\w+)([\\.]?\\w+)" ;
+// c помощью регулярного выражения проверят являится ли данная строка email
+        String regex = "(\\w+)([\\.-]?\\w+)*@(\\w+)([\\.]?\\w+)";
         Pattern p2 = Pattern.compile(regex);
         Matcher matcher = p2.matcher(email);
         if (matcher.matches()){
@@ -16,13 +16,21 @@ public class Main {
         }
     }
     public static void main(String[] args) {
+        System.out.println("Лабораторная работа 2\n" +
+                "Работу выполнили:\n " +
+                "Хмыренков Никита,\n " +
+                "Рязанский Максим,\n " +
+                "вариант 7");
         Scanner in = new Scanner(System.in);
         System.out.println("Введите Email");
         String s = in.nextLine();
-        if (Check_email(s)){
-            System.out.println("это Email");
-        }else{
-            System.out.println("Это не Email");
+        String[] words = s.split(",");
+        for (int i = 0; i< words.length; i++){
+            if (Check_email(words[i])){
+                System.out.println("это Email");
+            }else{
+                System.out.println("Это не Email");
+            }
         }
     }
 }
